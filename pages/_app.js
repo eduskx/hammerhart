8a6fee1b6c3f5a7d97544b464322853afe67c101
Moritz Biehl <moritz@Air-von-Moritz.fritz.box>
2024-08-05 17:24:04 +0200

    Fix bug from QA

--- pages/_app.js
+++ pages/_app.js
@@ -25,6 +25,7 @@ export default function App({ Component, pageProps }) {
           {...pageProps}
           projects={newProjects}
           onUpdateProject={handleUpdateProject}
+          setNewProjects={setNewProjects}
         />
       </div>
     </>
