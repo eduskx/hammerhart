10241a40a83980d376d31cc7eb4c123a80bebd7d
Moritz Biehl <moritz@Air-von-Moritz.fritz.box>
2024-08-08 10:07:55 +0200

    Have a default image when no image is selected

--- pages/api/upload/index.js
+++ pages/api/upload/index.js
@@ -13,21 +13,70 @@ export const config = {
   },
 };
 
-export default async function handler(request, response) {
-  if (request.method !== "POST") {
-    response.status(400).json({ message: "Method not allowed" });
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(400).json({ message: "Method not allowed" });
   }
-  const form = formidable({});
 
-  const [fields, files] = await form.parse(request);
-
-  const file = files.imageUrl[0];
-  const { newFilename, filepath } = file;
-
-  const result = await cloudinary.v2.uploader.upload(filepath, {
-    public_id: newFilename,
-    folder: "nf",
+  const form = formidable({
+    allowEmptyFiles: true,
+    minFileSize: 0,
   });
 
-  response.status(200).json(result);
+  form.parse(req, async (error, fields, files) => {
+    if (error) {
+      return res.status(500).json({ error: err.message });
+    }
+    console.log("FILeS", files);
+
+    let file;
+    if (
+      !files.imageUrl ||
+      files.imageUrl.length === 0 ||
+      files.imageUrl[0].size === 0
+    ) {
+      // Use a default image if no image is uploaded
+      file = {
+        filepath:
+          "https://images.unsplash.com/photo-1657434743747-07cbb4ddd5ba?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        newFilename: "default_image",
+      };
+    } else {
+      file = files.imageUrl[0];
+    }
+
+    const { newFilename, filepath } = file;
+
+    let result;
+    if (newFilename === "default_image") {
+      result = { url: filepath }; // Use the default image URL directly
+    } else {
+      result = await cloudinary.v2.uploader.upload(filepath, {
+        public_id: newFilename,
+        folder: "nf",
+      });
+    }
+
+    console.log("result", result);
+    return res.status(200).json(result);
+  });
 }
+
+// export default async function handler(request, response) {
+//   if (request.method !== "POST") {
+//     return response.status(400).json({ message: "Method not allowed" });
+//   }
+//   const form = formidable({});
+
+//   const [fields, files] = await form.parse(request);
+
+//   const file = files.imageUrl[0];
+//   const { newFilename, filepath } = file;
+
+//   const result = await cloudinary.v2.uploader.upload(filepath, {
+//     public_id: newFilename,
+//     folder: "nf",
+//   });
+
+//   response.status(200).json(result);
+// }
