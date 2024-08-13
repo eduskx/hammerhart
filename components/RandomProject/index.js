512a2e428ff2f704c6a4957909c8b9be86c82a15
SukranSari <sukran_sari@hotmail.de>
2024-08-13 16:10:12 +0200

    Debugging Random-Project

--- components/RandomProject/index.js
+++ components/RandomProject/index.js
@@ -1,19 +1,22 @@
+import { useEffect, useState } from "react";
 import ProjectCard from "../ProjectCard";
 
 export default function RandomProject({ projects }) {
+  const [randomProject, setRandomProject] = useState(null);
+  // inital rendering 0
+  // first rendering 1
+  useEffect(() => {
+    if (projects && projects.length > 0) {
+      const getRandomProject = (projects) => {
+        return projects[Math.floor(Math.random() * projects.length)];
+      };
+      setRandomProject(getRandomProject(projects));
+    }
+  }, [projects]);
+
   if (!projects) {
     return null;
   }
 
-  function getRandomProject(projects) {
-    return projects[Math.floor(Math.random() * projects.length)];
-  }
-
-  const randomProject = getRandomProject(projects);
-
-  return (
-    <div>
-      <ProjectCard project={randomProject} />
-    </div>
-  );
+  return <div>{randomProject && <ProjectCard project={randomProject} />}</div>;
 }
