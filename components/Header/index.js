894be4ba6bc362cb51334d20c85fa463605956d4
Kevin <kevinstein@gmx.net>
2024-08-16 23:53:16 +0200

    add menu butten and FadeOutNav animation

--- components/Header/index.js
+++ components/Header/index.js
@@ -27,55 +27,73 @@ export default function Header() {
       <StyledMenuIcon onClick={toggleMenu}>
         {isMenuOpen ? <MenuIconOpen /> : <MenuIcon />}
       </StyledMenuIcon>
-      {isMenuOpen && (
-        <StyledBackground onClick={closeMenu}>
-          <StyledDiv onClick={(event) => event.stopPropagation()}>
-            <StyledNavList>
-              <li>
-                <StyledListElements href="/bookmarks">My Projects</StyledListElements>
-              </li>
-            </StyledNavList>
-            <StyledSocalList>
-              <li>
-                <a href="http://www.facebook.com/" target="_blank">
-                  <StyledSocialMediaIcon>
-                    <Facebook />
-                  </StyledSocialMediaIcon>
-                </a>
-              </li>
-              <li>
-                <a href="http://www.instagram.com/" target="_blank">
-                  <StyledSocialMediaIcon>
-                    <Instagram />
-                  </StyledSocialMediaIcon>
-                </a>
-              </li>
-              <li>
-                <a href="http://www.x.com/" target="_blank">
-                  <StyledSocialMediaIcon>
-                    <X />
-                  </StyledSocialMediaIcon>
-                </a>
-              </li>
-              <li>
-                <a href="http://www.youtube.com/" target="_blank">
-                  <StyledSocialMediaIcon>
-                    <Youtube />
-                  </StyledSocialMediaIcon>
-                </a>
-              </li>
-            </StyledSocalList>
-          </StyledDiv>
-        </StyledBackground>
-      )}
+      <StyledBackground $isMenuOpen={isMenuOpen} onClick={closeMenu}>
+      </StyledBackground>
+        <StyledDropDownDiv $isMenuOpen={isMenuOpen} onClick={(event) => event.stopPropagation()}>
+          <StyledNavList>
+            <li>
+              <StyledListElements href="/">Home</StyledListElements>
+            </li>
+            <li>
+              <StyledListElements href="/bookmarks">
+                My Projects
+              </StyledListElements>
+            </li>
+          </StyledNavList>
+          <StyledSocalList>
+            <li>
+              <a href="http://www.facebook.com/" target="_blank">
+                <StyledSocialMediaIcon>
+                  <Facebook />
+                </StyledSocialMediaIcon>
+              </a>
+            </li>
+            <li>
+              <a href="http://www.instagram.com/" target="_blank">
+                <StyledSocialMediaIcon>
+                  <Instagram />
+                </StyledSocialMediaIcon>
+              </a>
+            </li>
+            <li>
+              <a href="http://www.x.com/" target="_blank">
+                <StyledSocialMediaIcon>
+                  <X />
+                </StyledSocialMediaIcon>
+              </a>
+            </li>
+            <li>
+              <a href="http://www.youtube.com/" target="_blank">
+                <StyledSocialMediaIcon>
+                  <Youtube />
+                </StyledSocialMediaIcon>
+              </a>
+            </li>
+          </StyledSocalList>
+        </StyledDropDownDiv>
+        <StyledDivBlocker></StyledDivBlocker>
     </StyledNavBar>
   );
 }
+
+const StyledDivBlocker = styled.div`
+  position: fixed;
+  right: 0;
+  top: 0;
+  width: 100%;
+  height: 80px;
+  background-color: #536f5f;
+  border-radius: 0 0 10px 10px;
+  z-index: 1;
+`;
+
+
 const StyledAnchor = styled.a`
   display: block;
   font-size: 0;
   z-index: 2;
-  `;
+`;
+
 const StyledBackground = styled.div`
   position: fixed;
   top: 0;
@@ -84,7 +102,9 @@ const StyledBackground = styled.div`
   height: 100vh;
   background-color: transparent;
   z-index: 1;
+  display: ${({$isMenuOpen}) => (!$isMenuOpen ? "none" : "block")};
 `;
+
 const StyledNavList = styled.ul`
   display: flex;
   flex-direction: column;
@@ -92,6 +112,7 @@ const StyledNavList = styled.ul`
   gap: 1rem;
   list-style: none;
   padding-right: 1.563rem;
+  z-index: 3;
 `;
 
 const StyledSocalList = styled(StyledNavList)`
@@ -104,6 +125,7 @@ const StyledSocalList = styled(StyledNavList)`
   justify-content: end;
   align-items: center;
 `;
+
 const StyledSocialMediaIcon = styled.div`
   &:hover {
     transition: all 0.5s ease;
@@ -117,11 +139,12 @@ const StyledNavBar = styled.div`
   right: 0;
   top: 0;
   width: 100%;
-  align-items: center;
   height: 80px;
+  background-color: #536f5f;
+  border-radius: 0 0 10px 10px;
+  align-items: center;
   padding: 16px 25px;
   justify-content: space-between;
- background-color: #536f5f;
   filter: drop-shadow(0px 4px 4px #00000049);
   z-index: 110;
 `;
@@ -147,7 +170,8 @@ const StyledMenuIcon = styled.div`
     transform: scale(1.1);
   }
 `;
-const StyledDiv = styled.div`
+
+const StyledDropDownDiv = styled.div`
   display: flex;
   flex-direction: column;
   align-items: end;
@@ -162,15 +186,26 @@ const StyledDiv = styled.div`
   background-color: #536f5f;
   border-radius: 0 0 10px 10px;
   z-index: 1;
-  animation: fadeInNav 0.4s ease 0s 1 normal forwards;
+  animation: ${({ $isMenuOpen }) => ($isMenuOpen ? 'fadeInNav' : 'fadeOutNav')} 0.4s ease-in-out 0s 1 normal forwards;
+
   @keyframes fadeInNav {
     0% {
       transform: scaleY(0);
       transform-origin: 100% 0%;
     }
-
     100% {
       transform: scaleY(1);
+      transform-origin: 100% 0%;
+    }
+  }
+
+  @keyframes fadeOutNav {
+    0% {
+      transform: scaleY(1);
+      transform-origin: 100% 0%;
+    }
+    100% {
+      transform: scaleY(0);
       transform-origin: 100% 0%;
     }
   }
@@ -184,7 +219,7 @@ const StyledListElements = styled.a`
   color: #f9f5eb;
   text-decoration: none;
   &:hover {
-    transition: all 0.5s ease;
+    transition: all 0.5s ease-in-out;
     transform: scale(1.1);
   }
 `;
