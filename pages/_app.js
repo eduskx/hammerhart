e8ce1d998fff8ee52e4994a45e12dad907d54298
SukranSari <sukran_sari@hotmail.de>
2024-08-21 17:27:14 +0200

    Refactor Delete-Function

--- pages/_app.js
+++ pages/_app.js
@@ -25,6 +25,7 @@ export default function App({ Component, pageProps }) {
   }
 
   function handleDeleteProject(id) {
+    localStorage.removeItem(`note-${id}`);
     setProjects(projects.filter((project) => project.id !== id));
   }
 
